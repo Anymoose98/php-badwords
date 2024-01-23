af9bdda3939f154fd9f8a961bbf81d8af76d0bd7
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-23 18:58:29 +0100

    Creato pagina iniziale

--- index.php
+++ index.php
@@ -8,12 +8,30 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <h1></h1>
-     <?php 
-    $prova = "prova prova";
-    $prova2= "Daniel";
-    echo '<h1 class="mt-3 mx-5">hello World '.$prova." ".$prova2."</h1>" 
-    
-    ?>   
+    <h1 class="text-center my-3">Censura Badwords</h1>
+    <div class="container">
+        <div class="row">
+            <form action="" method="GET" >
+                <div class="col-12 d-flex">
+
+                    <div class="col-6 m-3">
+                        <h3 class="text-center my-3">Testo da inserire</h3>
+                        <textarea class="form-control" name="testo" id="testo" cols="30" rows="3" placeholder="inserisci qui il testo" ></textarea>
+                    </div>
+
+                    <div class="col-6 m-3">
+                        <h3 class="text-center my-3">Parola da censurare</h3>
+                        <input type="text" name="parola" id="parola" class="form-control" placeholder="parola da censurare">
+                    </div>                    
+
+                </div>
+
+                <div class="col-12 text-center">
+                    <button type="submit" >invia</button>
+                </div>
+
+            </form>                    
+        </div>
+    </div>   
 </body>
 </html>
